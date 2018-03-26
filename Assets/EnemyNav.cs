455afd5e21e3edf1c58d750e4cfa5466f376de63
Chris Coetzee <christo.flames@gmail.com>
2018-03-26 19:32:34 +0200

    Prototype Iteration
    Further development on prototyping of enemy AI and Navigation.

---
+++ Assets/EnemyNav.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyNav : MonoBehaviour {
+    private NavMeshAgent navAgt;
+    public GameObject[] wayPoints;
+    public Vector3 dest;
+    private NavMeshPath path;
+    private Vector3[] pathTurns;
+    public GameObject turnTriggerPrefab;
+    private bool navigatingToPoint = false;
+    private bool navigatingPath = false;
+    private float direction;
+    // Use this for initialization
+    void Start() {
+        navAgt = GetComponent<NavMeshAgent>();
+        wayPoints = GameObject.FindGameObjectsWithTag("EnemyDestination");
+        path = navAgt.path;
+        if (!navigatingPath)
+            StartCoroutine(Navigate());
+    }
+
+    public IEnumerator Navigate()
+    {
+        if (navigatingPath == false)
+        {
+            navigatingPath = true;
+            print("newpath");
+            dest = wayPoints[Random.Range(0, wayPoints.Length-1)].transform.position;
+
+            navAgt.SetDestination(dest);
+
+            pathTurns = navAgt.path.corners;
+            foreach (GameObject obj in GameObject.FindGameObjectsWithTag("NavPathTurn"))
+            {
+                Destroy(obj);
+            }
+
+            foreach (Vector3 turn in pathTurns)
+            {
+                Transform turnPnt = gameObject.transform;
+                turnPnt.rotation = new Quaternion(0, 0, 0, 0);
+                turnPnt.localScale = new Vector3(1, 1, 1);
+                turnPnt.position = turn;
+
+                Instantiate(turnTriggerPrefab, turnPnt);
+            }
+
+            yield return new WaitForSeconds(1.0f);
+            yield return new WaitUntil(() => navAgt.remainingDistance <= 0.1f && navAgt.remainingDistance >= 0.0f);
+            navigatingPath = false;
+        }
+    }
+
+    void OnCollisionEnter(Collision colis)
+    {
+        
+    }
+    
+
+	// Update is called once per frame
+	void Update() { 
+        if(transform.rotation.eulerAngles.y >= 95 && transform.rotation.eulerAngles.y <= 100)
+        {
+            direction = 1;
+            navAgt.areaMask = 48;
+        }
+        else
+        if(transform.rotation.eulerAngles.y >= 170  && transform.rotation.eulerAngles.y <= 190)
+        {
+            direction = -1;
+            navAgt.areaMask = 40;
+        }
+        else
+        if(transform.rotation.eulerAngles.y >= 260 && transform.rotation.eulerAngles.y <= 280)
+        {
+            direction = -2;
+            navAgt.areaMask = 48;
+        }
+        else
+        if(transform.rotation.eulerAngles.y >= 350 && transform.rotation.eulerAngles.y <= 10)
+        {
+            direction = -2;
+            navAgt.areaMask = 40;
+        }
+        print(navAgt.remainingDistance);
+        
+        if (!navigatingPath)
+        {
+            StartCoroutine(Navigate());
+
+            
+        }
+        
+    }
+            
+}
---
+++ Assets/EnemySpawner.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+    {
+        public GameObject enemy;
+        private GameObject[] enemyscurr = new GameObject[5];
+        private Transform loc;
+        public int enemysAlive = 0;
+        //public GameObject EnemySpawns;
+        public Transform[] spawnlocs;
+        public int enemiesToSpawn = 2;
+
+        // Use this for initialization
+        void Start()
+        {
+             GameObject[] sps = GameObject.FindGameObjectsWithTag("EnemySpawnPoint");
+                int counter = 0;
+                foreach (GameObject sp in sps)
+                {
+                    if (counter != sps.Length)
+                    {
+                        spawnlocs[counter] = sp.transform;
+                        counter++;
+                    }
+                }
+
+            int ensleft = enemiesToSpawn;
+            while(ensleft > 0)
+            {
+                Trigger_spawn();
+                ensleft--;
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (enemysAlive == 0)
+            {
+                enemyscurr = new GameObject[5];
+                int ensleft = enemiesToSpawn;
+                while (ensleft > 0)
+                {
+                    Trigger_spawn();
+                }
+            }
+        }
+
+        public void enemyKilled()
+        {
+
+        }
+
+        public void Trigger_spawn()
+        {
+            int num = Random.Range(0, spawnlocs.Length - 1);
+            loc = spawnlocs[num];
+            GameObject current = Instantiate(enemy, loc, true);
+        }
+
+    }
